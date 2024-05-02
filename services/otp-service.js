d4fafba07910518ef6c7732abe4d069f646921ac
Xdahiya <xdahiya@gmail.com>
2024-05-02 13:16:23 +0530

    initial

---
+++ services/otp-service.js
@@ -0,0 +1,35 @@
+const crypto = require("crypto");
+const hashService = require("./hash-service");
+const dotenv = require("dotenv");
+
+dotenv.config();
+const smsSid = process.env.SMS_SID;
+const smsAuthToken = process.env.SMS_AUTH_TOKEN;
+const twilio = require("twilio")(smsSid, smsAuthToken, {
+  lazyLoading: true,
+});
+
+class OtpService {
+  async generateOtp() {
+    const otp = crypto.randomInt(1000, 9999);
+    console.log(`Your codershouse OTP is ${otp}`);
+
+    return otp;
+  }
+
+  async sendBySms(phone, otp) {
+    console.log(`Your codershouse OTP is ${otp}`);
+    return await twilio.messages.create({
+      to: phone,
+      from: process.env.SMS_FROM_NUMBER,
+      body: `Your codershouse OTP is ${otp}`,
+    });
+  }
+
+  verifyOtp(hashedOtp, data) {
+    let computedHash = hashService.hashOtp(data);
+    return computedHash === hashedOtp;
+  }
+}
+
+module.exports = new OtpService();
